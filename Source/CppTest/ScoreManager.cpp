fda5a53cd97274a462e27c7cdf8d03eba7932ff9
AJ-Ora <antti-jussioravainen@kamk.fi>
2018-11-02 13:43:17 +0200

    Modified score manager to have static (global) functionality.

--- Source/CppTest/ScoreManager.cpp
+++ Source/CppTest/ScoreManager.cpp
@@ -1,20 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ScoreManager.h"
-
-UScoreManager::UScoreManager()
-{
-	uint64 CurrentScore = 0ULL;
-	uint64 SessionHighScore = 0ULL;
-}
-
-UScoreManager::~UScoreManager()
-{
-}
+#include <EngineGlobals.h>
+#include <Runtime/Engine/Classes/Engine/Engine.h>
 
 void UScoreManager::AddPoints(int points)
 {
 	CurrentScore += (uint64)FMath::Abs(points);
+	GEngine->AddOnScreenDebugMessage(69, 5.0f, FColor::Green, FString::Printf(TEXT("Current score: %i"), CurrentScore));
 }
 
 void UScoreManager::ResetCurrentScore()

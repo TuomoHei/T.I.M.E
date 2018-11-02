b7db89920a4a3a6638eccaf53460aaa5251598ec
AJ-Ora <antti-jussioravainen@kamk.fi>
2018-11-02 15:55:48 +0200

    Added multiplier system to ScoreManager.

--- Source/CppTest/ScoreManager.cpp
+++ Source/CppTest/ScoreManager.cpp
@@ -4,15 +4,64 @@
 #include <EngineGlobals.h>
 #include <Runtime/Engine/Classes/Engine/Engine.h>
 
+UScoreManager::UScoreManager()
+{
+	MultiplierTimer = 0.0f;
+	ScoreMultiplier = 1;
+	IsPaused = false;
+}
+
+void UScoreManager::Tick(float DeltaTime)
+{
+	if (!IsPaused && MultiplierTimer > 0.0f)
+	{
+		MultiplierTimer -= DeltaTime;
+
+		if (MultiplierTimer < 0.0f)
+		{
+			if (ScoreMultiplier > 2)
+			{
+				MultiplierTimer = MultiplierTimerAddition;
+			}
+			else
+			{
+				MultiplierTimer = 0.0f;
+			}
+			ScoreMultiplier--;
+		}
+	}
+
+	GEngine->AddOnScreenDebugMessage(1336, 5.0f, FColor::White, FString::Printf(TEXT("Score multiplier: %i"), ScoreMultiplier));
+	GEngine->AddOnScreenDebugMessage(1337, 5.0f, FColor::White, FString::Printf(TEXT("Current time: %f"), MultiplierTimer));
+}
+
+bool UScoreManager::IsTickableInEditor() const
+{
+	return false;
+}
+
+bool UScoreManager::IsTickableWhenPaused() const
+{
+	return false;
+}
+
+TStatId UScoreManager::GetStatId() const
+{
+	return TStatId();
+}
+
 void UScoreManager::AddPoints(int points)
 {
-	CurrentScore += (uint64)FMath::Abs(points);
+	CurrentScore += (uint64)FMath::Abs(points * ScoreMultiplier++);
+	MultiplierTimer = MultiplierTimerAddition;
 	GEngine->AddOnScreenDebugMessage(69, 5.0f, FColor::Green, FString::Printf(TEXT("Current score: %i"), CurrentScore));
 }
 
 void UScoreManager::ResetCurrentScore()
 {
 	CurrentScore = 0ULL;
+	MultiplierTimer = 0.0f;
+	ScoreMultiplier = 1;
 }
 
 uint64 UScoreManager::GetSessionHighScore()
--- Source/CppTest/ScoreManager.h
+++ Source/CppTest/ScoreManager.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "Runtime/Engine/Public/Tickable.h"
 #include "ScoreManager.generated.h"
 
 /**
@@ -12,15 +13,24 @@
 
 static uint64 CurrentScore = 0ULL;
 static uint64 SessionHighScore = 0ULL;
+static float MultiplierTimer;
+static const float MultiplierTimerAddition = 5.0f;
+static int ScoreMultiplier;
+static bool IsPaused;
 
 UCLASS()
-class CPPTEST_API UScoreManager : public UObject
+class CPPTEST_API UScoreManager : public UObject, public FTickableGameObject
 {
 	GENERATED_BODY()
 	
 public:
-	UScoreManager() {}
+	UScoreManager();
 	~UScoreManager() {}
+
+	void Tick(float DeltaTime) override;
+	bool IsTickableInEditor() const override;
+	bool IsTickableWhenPaused() const override;
+	TStatId GetStatId() const override;
 
 	static void AddPoints(int points);
 	static void ResetCurrentScore();
